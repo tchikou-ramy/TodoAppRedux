2778238e6ccf6d83c7020598bfbc4a810b33e561
tchikou-ramy <ramytch10@gmail.com>
2022-10-13 17:06:17 +0100

    first commit

---
+++ src/Components/TaskList.js
@@ -0,0 +1,17 @@
+
+import { useSelector } from 'react-redux'
+import TaskItem from './TaskItem'
+
+const TaskList = () => {
+  const taskList = useSelector(state => state.TaskReducer.taskList)
+  return (
+    <div>
+      {
+        taskList.map((el) => <TaskItem todo={el} />
+        )
+      }
+    </div>
+  )
+}
+
+export default TaskList
